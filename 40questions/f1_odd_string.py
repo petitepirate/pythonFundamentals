9fe36f13789b384a5eeca9114377beb1e2e644bb
megan <msmeganmcmanus@gmail.com>
2020-08-04 15:03:00 -0700

    further study finished, OOP started

--- 40questions/f1_odd_string.py
+++ 40questions/f1_odd_string.py
@@ -28,4 +28,12 @@ def is_odd_string(word):
         True
     """
 
-    # Hint: you may find the ord() function useful here
+    # to find the char position, we'll change it's ordinal ASCII number into
+    # a 1-based number ("a" = 1, "b" = 2). To do that, let's subtract
+    # this from it
+
+    DIFF = ord("a") - 1
+
+    total = sum((ord(c) - DIFF) for c in word.lower())
+
+    return total % 2 == 1
