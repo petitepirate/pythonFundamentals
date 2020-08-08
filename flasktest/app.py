d2e2995cf679577e389269a4f681dc5ccdece53e
root <root@DESKTOP-NIR74OK.localdomain>
2020-08-08 16:53:49 -0700

    small code addition

--- flasktest/app.py
+++ flasktest/app.py
@@ -1,6 +1,17 @@
 from flask import Flask
 app = Flask(__name__)
 
+
 @app.route('/hello')
 def say_hello():
     return "HelloThere!"
+
+
+print("This line will be printed.")
+
+x = 1
+if x == 1:
+    # indented four spaces
+    print("x is 1.")
+
+    print("Goodbye, World!")
